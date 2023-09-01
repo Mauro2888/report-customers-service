1f4585f6235d31514eb60aec438ff88940302d50
mauro <mauro.dev88@gmail.com>
2023-09-01 17:48:48 +0200

    added method for save

--- src/main/java/report/repository/custom/BaseRepository.java
+++ src/main/java/report/repository/custom/BaseRepository.java
@@ -8,4 +8,7 @@ public interface BaseRepository<E> {
     <T extends E> T findById(Class<T> tClass, UUID id);
 
     <T extends E> List<T> findAll(Class<T> tClass);
+
+    <T extends E> T save(T entity);
+
 }
